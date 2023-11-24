a3f5c6b6e78b9abe2d9656be3eb926f22229ba14
Lee-Jungwoo <dlwjddn0225@gmail.com>
2023-11-24 14:22:37 +0900

    still figuring out what deleted means '

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,18 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    auto window = sf::RenderWindow{ { 1280u, 720u }, "CMake SFML Project" };
+    // sf::RenderWindow window;
+    
     window.setFramerateLimit(144);
-
+    
     while (window.isOpen())
     {
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
             if (event.type == sf::Event::Closed)
             {
+                
                 window.close();
             }
         }
