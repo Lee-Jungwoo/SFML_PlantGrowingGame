85d0989f125fd633e19e0c66eeaed4b8df6afe96
Lee-Jungwoo <dlwjddn0225@gmail.com>
2023-12-04 15:53:49 +0900

    original main file deprecated

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -40,6 +40,8 @@ int main()
                     B.ChangeMode(pos);
                     break;
                 }
+                default:
+                    break;
                 }
             }
        
